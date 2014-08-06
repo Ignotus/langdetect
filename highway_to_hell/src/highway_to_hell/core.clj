fefa83fff9ac605301c77d30c99973e6c418bdcb
Minh Ngo <nlminhtl@gmail.com>
2014-08-06 19:17:10 +0300

    Very simple parser into words

--- highway_to_hell/src/highway_to_hell/core.clj
+++ highway_to_hell/src/highway_to_hell/core.clj
@@ -2,7 +2,11 @@
   (:use [highway-to-hell.wiki])
   (:gen-class))
 
+(defn parse-data [data]
+  (clojure.string/split data #"\s"))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [page-name & args]
-  (println (remove-html-tags (get-text :uk page-name))))
+  (println (clojure.string/join ", " (parse-data (clojure.string/lower-case (remove-html-tags (get-text :uk page-name)))))))
