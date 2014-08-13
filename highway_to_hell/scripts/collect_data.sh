37687597dad6a4e2644091d2634bd87994c42077
Minh Ngo <nlminhtl@gmail.com>
2014-08-13 21:37:54 +0300

    A bit optimization

--- highway_to_hell/scripts/collect_data.sh
+++ highway_to_hell/scripts/collect_data.sh
@@ -2,6 +2,33 @@
 
 mkdir -p ../data
 
+filename=enwiki-20140811-pages-articles7.xml-p000305002p000464996.bz2
+if [ ! -f $filename ]
+then
+  wget https://dumps.wikimedia.org/enwiki/20140811/$filename
+fi
+
+bzip2 -dc $filename | python WikiExtractor.py -cb 250K -o ../data/en &
+
+
+filename=afwiki-20140811-pages-articles.xml.bz2
+if [ ! -f $filename ]
+then
+  wget https://dumps.wikimedia.org/afwiki/20140811/$filename
+fi
+
+bzip2 -dc $filename | python WikiExtractor.py -cb 250K -o ../data/af &
+
+
+filename=ruwiki-20140727-pages-articles1.xml.bz2
+if [ ! -f $filename ]
+then
+  wget https://dumps.wikimedia.org/ruwiki/20140727/$filename
+fi
+
+bzip2 -dc $filename | python WikiExtractor.py -cb 250K -o ../data/ru &
+
+
 filename=ukwiki-20140722-pages-articles.xml.bz2
 if [ ! -f $filename ]
 then
@@ -20,30 +47,6 @@ fi
 bzip2 -dc $filename | python WikiExtractor.py -cb 250K -o ../data/de &
 
 
-filename=ruwiki-20140727-pages-meta-current1.xml.bz2
-if [ ! -f $filename ]
-then
-  wget https://dumps.wikimedia.org/ruwiki/20140727/$filename
-fi
-
-bzip2 -dc $filename | python WikiExtractor.py -cb 250K -o ../data/ru &
-
-filename=afwiki-20140811-pages-articles.xml.bz2
-if [ ! -f $filename ]
-then
-  wget https://dumps.wikimedia.org/afwiki/20140811/$filename
-fi
-
-bzip2 -dc $filename | python WikiExtractor.py -cb 250K -o af &
-
-filename=enwiki-20140811-pages-articles7.xml-p000305002p000464996.bz2
-if [ ! -f $filename ]
-then
-  wget https://dumps.wikimedia.org/enwiki/20140811/$filename
-fi
-
-bzip2 -dc $filename | python WikiExtractor.py -cb 250K -o ../data/en &
-
 filename=plwiki-20140802-pages-articles3.xml.bz2
 if [ ! -f $filename ]
 then
@@ -51,6 +54,7 @@ then
 fi
 
 bzip2 -dc $filename | python WikiExtractor.py -cb 250K -o ../data/pl &
+
 
 filename=bgwiki-20140728-pages-articles.xml.bz2
 if [ ! -f $filename ]
